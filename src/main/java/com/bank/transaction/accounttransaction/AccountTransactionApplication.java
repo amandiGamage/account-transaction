3ce0f571f3ad493d80743b9b36e4aa84fd372f02
Amandi Imasha <Amandiimasha43@gmail.com>
2018-09-29 14:34:57 +0530

    Initial

---
+++ src/main/java/com/bank/transaction/accounttransaction/AccountTransactionApplication.java
@@ -0,0 +1,12 @@
+package com.bank.transaction.accounttransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountTransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountTransactionApplication.class, args);
+	}
+}
---
+++ src/test/java/com/bank/transaction/accounttransaction/AccountTransactionApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bank.transaction.accounttransaction;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AccountTransactionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
